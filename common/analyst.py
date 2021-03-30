6209b0032bf3da4eaef89d466a72e2b0a216a4c1
namuan <575441+namuan@users.noreply.github.com>
2021-03-30 18:45:30 +0100

    Calculating SMA and EMA over multiple periods

--- common/analyst.py
+++ common/analyst.py
@@ -21,14 +21,31 @@ def enrich_data(ticker_symbol, is_etf=False):
         return {}
 
     last_close_date = ticker_df.index[-1]
+    last_trading_day = ticker_df.iloc[-1]
     stock_data_52_weeks = ticker_df["close"][-256:]
     high_52_weeks = stock_data_52_weeks.max()
     low_52_weeks = stock_data_52_weeks.min()
-    return {
+    data_row = {
         "symbol": ticker_symbol,
         "is_etf": is_etf,
-        "last_close": ticker_df["close"].iloc[-1],
+        "last_close": last_trading_day["close"],
         "last_close_date": last_close_date,
         "high_52_weeks": high_52_weeks,
         "low_52_weeks": low_52_weeks,
+        "last_volume": last_trading_day["volume"],
+        "last_high": last_trading_day["high"],
+        "last_low": last_trading_day["low"],
+        "boll": ticker_df["boll"].iloc[-1],
+        "boll_ub": ticker_df["boll_ub"].iloc[-1],
+        "boll_lb": ticker_df["boll_lb"].iloc[-1],
     }
+
+    # Simple Moving Average
+    for ma in [10, 20, 30, 50, 100, 200]:
+        data_row[f"ma_{ma}"] = ticker_df[f"close_{ma}_sma"].iloc[-1]
+
+    # Exp Moving Average
+    for ema in [10, 20, 30, 50, 100, 200]:
+        data_row[f"ema_{ema}"] = ticker_df[f"close_{ema}_ema"].iloc[-1]
+
+    return data_row
