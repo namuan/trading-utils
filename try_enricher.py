a61cf1ec3b6b495744ce6fb83e70acc06b118fc7
namuan <575441+namuan@users.noreply.github.com>
2021-04-25 12:35:33 +0100

    Add a test script to verify analyst calculations without running the whole enrichment process

---
+++ try_enricher.py
@@ -0,0 +1,29 @@
+import json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from common.analyst import enrich_data
+
+plt.ioff()
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+ticker = "COIN"
+
+# weekly_options.set_index('Symbol', inplace=True)
+# cboe_options = pd.read_csv(f"data/cboesymboldirequityindex.csv")
+# print(has_options('AAPL'))
+data = enrich_data(ticker)
+keys = list(data.keys())
+print(json.dumps(keys, indent=4))
+print(data)
+# weekly_ticker_candles = convert_to_weekly(df)
+#
+# for wp in [4, 8]:
+#     df[["max_weekly_{}".format(wp), "max_weekly_{}_at".format(wp)]] = max_weekly(
+#         weekly_ticker_candles, week_until=wp
+#     )
+
+# print(df)
