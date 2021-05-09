52e22382b35aeaf810cee78c6e0ae51f8a755469
namuan <575441+namuan@users.noreply.github.com>
2021-05-09 16:37:41 +0100

    Simple script to test integration with locally running Trader workstation

---
+++ ib_spike.py
@@ -0,0 +1,17 @@
+# pip install ib_insync
+# https://ib-insync.readthedocs.io/recipes.html
+
+from ib_insync import *
+# util.startLoop()  # uncomment this line when in a notebook
+
+ib = IB()
+ib.connect('127.0.0.1', 7497, clientId=1)
+
+contract = Stock('TSLA', 'SMART', 'USD')
+bars = ib.reqHistoricalData(
+    contract, endDateTime='', durationStr='30 D',
+    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+
+# convert to pandas dataframe:
+df = util.df(bars)
+print(df)
