ec460c72c5685aab9a3fe92fd37f2781d9b5bc19
namuan <575441+namuan@users.noreply.github.com>
2021-06-06 21:04:46 +0100

    Initial setup to add a new bot to alert on stock prices

--- Makefile
+++ Makefile
@@ -55,6 +55,7 @@ deploy: clean ## Copies any changed file to the server
 		options_price_tracker.py \
 		tele_stock_rider_bot.py \
 		tele_theta_gang_bot.py \
+		tele_stock_alerts_bot.py \
 		webpages.txt \
 		twitter_furus.py \
 		twitter_furus_accounts.txt \
---
+++ tele_stock_alerts_bot.py
@@ -0,0 +1,50 @@
+import logging
+
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+
+from common.bot_wrapper import start, help_command
+from common.environment import (
+    TELEGRAM_STOCK_ALERT_BOT,
+)
+from common.logger import init_logging
+
+
+def handle_cmd(update: Update, context: CallbackContext) -> None:
+    message_text: str = update.message.text
+    if len(message_text.split(" ")) < 3:
+        print(f"More information provided so it could be for a different bot: {message_text}")
+        return
+
+    maybe_symbol, criteria, threshold = message_text.split(" ")
+    if maybe_symbol.startswith("$"):
+        ticker = maybe_symbol[1:]
+        update.message.reply_text(f"Set up alert when #{ticker} {criteria} {threshold}", quote=True)
+
+
+def main():
+    """Start the bot."""
+    logging.info("Starting tele-stock-alerts-bot")
+    updater = Updater(TELEGRAM_STOCK_ALERT_BOT, use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_cmd))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == "__main__":
+    init_logging()
+    main()
