e1edceda0982aa500a60f23bae57bca0cafb4f2a
namuan <575441+namuan@users.noreply.github.com>
2023-09-23 21:35:30 +0100

    feat: RSI with Scale In

--- back-trader-tutorial.py
+++ back-trader-tutorial.py
@@ -3,30 +3,63 @@ from pathlib import Path
 
 import backtrader as bt
 
+# Factor to percent of investment
+scale_in = {1: 0.05, 2: 0.15, 3: 0.3, 4: 0.5}
+
 
 class TestStrategy(bt.Strategy):
-    params = (("ma_period", 14), ("print_log", False))
+    params = (
+        ("initial_investment", 10000.0),
+        ("rsi_period", 14),
+        ("rsi_lower", 30),
+        ("rsi_upper", 70),
+        ("print_log", True),
+    )
 
     def __init__(self):
         self.bar_executed = None
         self.data_close = self.datas[0].close
         self.order = None
-        self.sma = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=self.params.ma_period
+        self.trades_holding = 0
+        self.scale_in_step = 1
+        self.total_stocks_purchased = 0
+        self.rsi = bt.indicators.RSI(
+            self.datas[0],
+            period=self.params.rsi_period,
+            upperband=self.params.rsi_upper,
+            lowerband=self.params.rsi_lower,
         )
-        # rsi = bt.indicators.RSI(self.datas[0])
-        # bt.indicators.SmoothedMovingAverage(rsi, period=10)
 
     def next(self):
-        # self.log(f"Close, {self.data_close[0]}")
-        if not self.position:
-            if self.data_close[0] > self.sma[0]:
-                self.log(f"Buy Create, Close {self.data_close[0]} > SMA {self.sma[0]}")
-                self.buy()
+        if self.rsi[0] < self.params.rsi_lower:
+            emoji = "👍"
+        elif self.rsi[0] > self.params.rsi_upper:
+            emoji = "👎"
         else:
-            if self.data_close[0] < self.sma[0]:
-                self.log(f"Sell Create, Close {self.data_close[0]} < SMA {self.sma[0]}")
-                self.sell()
+            emoji = "❌"
+
+        self.log(f"Close, {self.data_close[0]}, RSI = {self.rsi[0]:.2f} {emoji}")
+        if self.scale_in_step <= len(scale_in):
+            if self.rsi[0] < self.params.rsi_lower:
+                current_scale_factor = scale_in[self.scale_in_step]
+                stocks_to_purchase = (
+                    self.params.initial_investment * current_scale_factor
+                ) / self.data_close[0]
+
+                self.log(
+                    f"Buy Create {stocks_to_purchase:.2f} @ Close {self.data_close[0]} > RSI {self.rsi[0]}"
+                )
+                self.order = self.buy(size=stocks_to_purchase)
+                self.scale_in_step += 1
+                self.trades_holding += 1
+                self.total_stocks_purchased += stocks_to_purchase
+
+        if self.trades_holding > 0 and self.rsi[0] > self.params.rsi_upper:
+            self.log(f"Sell Create, Close {self.data_close[0]} < RSI {self.rsi[0]}")
+            self.order = self.sell(size=self.total_stocks_purchased)
+            self.trades_holding = 0
+            self.scale_in_step = 1
+            self.total_stocks_purchased = 0
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -35,14 +68,14 @@ class TestStrategy(bt.Strategy):
         if order.status in [order.Completed]:
             if order.isbuy():
                 self.log(
-                    f"Buy Executed, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm:.2f}"
+                    f"BUY Executed, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm:.2f}"
                 )
             elif order.issell():
                 self.log(
-                    f"Sell Executed, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm:.2f}"
+                    f"SELL Executed, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm:.2f}"
                 )
-            self.bar_executed = len(self)
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            print(order)
             self.log("Order Canceled/Margin/Rejected")
 
         self.order = None
@@ -55,36 +88,43 @@ class TestStrategy(bt.Strategy):
 
     def stop(self):
         self.log(
-            f"(MA Period {self.params.ma_period:2d}) Ending Value {self.broker.getvalue():.2f}",
+            f"(RSI Period {self.params.rsi_period:2d}) (Upper {self.params.rsi_upper} : Lower {self.params.rsi_lower}) Ending Value {self.broker.getvalue():.2f}",
             do_print=True,
         )
 
     def log(self, txt, dt=None, do_print=False):
-        if self.params.print_log or do_print:
-            dt = dt or self.datas[0].datetime.date(0)
-            print(f"{dt.isoformat()}, {txt}")
+        # if self.params.print_log or do_print:
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f"{dt.isoformat()}, {txt}")
 
 
 if __name__ == "__main__":
     cerebro = bt.Cerebro()
+    initial_investment = 10000.0
     # cerebro.optstrategy(TestStrategy, ma_period=range(5, 30))
-    cerebro.addstrategy(TestStrategy, ma_period=20)
+    cerebro.addstrategy(
+        TestStrategy,
+        initial_investment=initial_investment,
+        rsi_period=4,
+        rsi_lower=20,
+        rsi_upper=75,
+    )
 
     # Load feed
-    data_path = Path.cwd().joinpath("output").joinpath("AAPL.csv")
+    data_path = Path.cwd().joinpath("output").joinpath("TSLA.csv")
 
     data = bt.feeds.YahooFinanceCSVData(
         dataname=data_path,
-        fromdate=datetime.datetime(2004, 1, 1),
-        todate=datetime.datetime(2023, 1, 3),
-        reverse=False,
+        fromdate=datetime.datetime(2019, 1, 1),
+        todate=datetime.datetime(2023, 1, 1),
+        # reverse=False,
     )
 
     cerebro.adddata(data)
-    cerebro.broker.setcash(10000.0)
+    cerebro.broker.setcash(initial_investment)
     cerebro.broker.setcommission(commission=0.001)
 
     print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
     cerebro.run(maxcpus=1)
     print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
-    # cerebro.plot()
+    cerebro.plot()
