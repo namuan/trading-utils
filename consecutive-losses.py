bb63cdb8088616b757d8375bff62047d3b2bb989
namuan <575441+namuan@users.noreply.github.com>
2023-09-25 20:04:56 +0100

    Refactored code to calculate and print max consecutive lower closes and max percentage drop for both daily and weekly data.

--- consecutive-losses.py
+++ consecutive-losses.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def calculate_consecutive_lower_closes(df):
     df["Lower_Close"] = df["Close"].diff() < 0
     df["Consecutive_Lower_Close"] = 0
@@ -9,6 +10,7 @@ def calculate_consecutive_lower_closes(df):
                 df.loc[i - 1, "Consecutive_Lower_Close"] + 1
             )
     return df
+
 
 def find_max_consecutive_lower_closes(df):
     max_consecutive_lower_close = df["Consecutive_Lower_Close"].max()
@@ -22,6 +24,7 @@ def find_max_consecutive_lower_closes(df):
         consecutive_lower_close_periods.append((starting_date, ending_date))
     return max_consecutive_lower_close, consecutive_lower_close_periods
 
+
 def calculate_max_percentage_drop(df, consecutive_lower_close_periods):
     max_percentage_drop = 0.0
     for start_date, end_date in consecutive_lower_close_periods:
@@ -33,8 +36,10 @@ def calculate_max_percentage_drop(df, consecutive_lower_close_periods):
             max_percentage_drop = max(max_percentage_drop, percentage_drop)
     return max_percentage_drop
 
+
 def resample_data(df, freq):
     return df.resample(freq, on="Date").last().reset_index()
+
 
 def main():
     # Load the CSV file
@@ -43,8 +48,13 @@ def main():
     df["Date"] = pd.to_datetime(df["Date"])
 
     df = calculate_consecutive_lower_closes(df)
-    max_consecutive_lower_close, consecutive_lower_close_periods = find_max_consecutive_lower_closes(df)
-    max_percentage_drop = calculate_max_percentage_drop(df, consecutive_lower_close_periods)
+    (
+        max_consecutive_lower_close,
+        consecutive_lower_close_periods,
+    ) = find_max_consecutive_lower_closes(df)
+    max_percentage_drop = calculate_max_percentage_drop(
+        df, consecutive_lower_close_periods
+    )
 
     print("Max Consecutive Lower Closes:", max_consecutive_lower_close)
     print("Periods when it happened:", consecutive_lower_close_periods)
@@ -52,12 +62,21 @@ def main():
 
     df_weekly = resample_data(df, "W")
     df_weekly = calculate_consecutive_lower_closes(df_weekly)
-    max_consecutive_lower_close_weekly, consecutive_lower_close_periods_weekly = find_max_consecutive_lower_closes(df_weekly)
-    max_percentage_drop_weekly = calculate_max_percentage_drop(df, consecutive_lower_close_periods_weekly)
+    (
+        max_consecutive_lower_close_weekly,
+        consecutive_lower_close_periods_weekly,
+    ) = find_max_consecutive_lower_closes(df_weekly)
+    max_percentage_drop_weekly = calculate_max_percentage_drop(
+        df, consecutive_lower_close_periods_weekly
+    )
 
     print("Max Consecutive Lower Closes (Weekly):", max_consecutive_lower_close_weekly)
     print("Periods when it happened (Weekly):", consecutive_lower_close_periods_weekly)
-    print("Max Percentage Drop in Consecutive Weekly Lower Closes:", max_percentage_drop_weekly)
+    print(
+        "Max Percentage Drop in Consecutive Weekly Lower Closes:",
+        max_percentage_drop_weekly,
+    )
+
 
 if __name__ == "__main__":
     main()
