696062b7c323395c5667a06a687f5013ad8fda4d
namuan <575441+namuan@users.noreply.github.com>
2023-10-01 21:02:13 +0100

    refactor: Clean up and enable scripts

--- scripts/stop_apps.sh
+++ scripts/stop_apps.sh
@@ -1,12 +1,4 @@
 cd $1 || exit
 bash ./scripts/stop_screen.sh tele-links
-#bash ./scripts/stop_screen.sh tele-twitter
-bash ./scripts/stop_screen.sh tele-furus
-bash ./scripts/stop_screen.sh options-price-tracker
-bash ./scripts/stop_screen.sh tele-stock-rider-bot
-bash ./scripts/stop_screen.sh tele-theta-gang-bot
-bash ./scripts/stop_screen.sh tele-stock-alerts-bot
-#bash ./scripts/stop_screen.sh 1-usdt-trade-bot
-#bash ./scripts/stop_screen.sh 2-usdt-trade-bot
-bash ./scripts/stop_screen.sh strat-usdt
 bash ./scripts/stop_screen.sh tele-spy-trade-bot
+bash ./scripts/stop_screen.sh tele-theta-gang-bot
