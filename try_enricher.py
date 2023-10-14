b7807ff0fa3eb2f30be1a3facb74e8d7a9de38bb
namuan <575441+namuan@users.noreply.github.com>
2023-10-14 11:54:03 +0100

    feat: Try enricher using any provided symbol

--- try_enricher.py
+++ try_enricher.py
@@ -1,31 +1,76 @@
-import json
+#!/usr/bin/env python3
+"""
+Download Stock OLHC data from Yahoo Finance and generate stats
+
+Usage:
+$ ./try_enricher.py --help
+$ ./try_enricher.py --symbol TSLA
+"""
+import logging
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from common.analyst import fetch_data_from_cache
+from common.analyst import fetch_data_on_demand
 
 plt.ioff()
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
 
-ticker = "AAPL"
 
-# weekly_options.set_index('Symbol', inplace=True)
-# cboe_options = pd.read_csv(f"data/cboesymboldirequityindex.csv")
-# print(has_options('AAPL'))
-# data, ticker_df = fetch_data_on_demand(ticker)
-data = fetch_data_from_cache(ticker, is_etf=False)
-key_values = list([(k, data[k]) for k in data.keys()])
-for kv in key_values:
-    print(kv)
+def setup_logging(verbosity):
+    logging_level = logging.WARNING
+    if verbosity == 1:
+        logging_level = logging.INFO
+    elif verbosity >= 2:
+        logging_level = logging.DEBUG
 
-# weekly_ticker_candles = convert_to_weekly(df)
-#
-# for wp in [4, 8]:
-#     df[["max_weekly_{}".format(wp), "max_weekly_{}_at".format(wp)]] = max_weekly(
-#         weekly_ticker_candles, week_until=wp
-#     )
+    logging.basicConfig(
+        handlers=[
+            logging.StreamHandler(),
+        ],
+        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging_level,
+    )
+    logging.captureWarnings(capture=True)
 
-# print(df)
+
+def parse_args():
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        dest="verbose",
+        help="Increase verbosity of logging output",
+    )
+    parser.add_argument(
+        "-s",
+        "--symbol",
+        type=str,
+        default="AAPL",
+        help="Stock symbol (default: AAPL)",
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    ticker = args.symbol
+    data, ticker_df = fetch_data_on_demand(ticker)
+
+    # data = fetch_data_from_cache(ticker, is_etf=False)
+    key_values = list([(k, data[k]) for k in data.keys()])
+    for kv in key_values:
+        print(kv)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    setup_logging(args.verbose)
+    main(args)
