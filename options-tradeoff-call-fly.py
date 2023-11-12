3a156a0742fc3d83cc312f73e53c1d129914c74f
namuan <575441+namuan@users.noreply.github.com>
2023-11-12 17:28:04 +0000

    chore: Setup Spy trade bot

--- options-tradeoff-call-fly.py
+++ options-tradeoff-call-fly.py
@@ -24,67 +24,83 @@ premium_short_call = 1.80
 sT = np.arange(10, 60, 1)
 
 # OTM Strike Long Call Payoff
-lower_strike_long_call_payoff = call_payoff(sT, lower_strike_price_long_call, premium_lower_strike_long_call)
+lower_strike_long_call_payoff = call_payoff(
+    sT, lower_strike_price_long_call, premium_lower_strike_long_call
+)
 
 fig, ax = plt.subplots()
-ax.spines['bottom'].set_position('zero')
-ax.plot(sT,lower_strike_long_call_payoff, color='g')
-ax.set_title('LONG 30 Strike Call')
-plt.xlabel('Stock Price')
-plt.ylabel('Profit & Loss')
+ax.spines["bottom"].set_position("zero")
+ax.plot(sT, lower_strike_long_call_payoff, color="g")
+ax.set_title("LONG 30 Strike Call")
+plt.xlabel("Stock Price")
+plt.ylabel("Profit & Loss")
 
 plt.show()
 
 # Higher Strike Long Call Payoff
-higher_strike_long_call_payoff = call_payoff(sT, higher_strike_price_long_call, premium_higher_strike_long_call)
+higher_strike_long_call_payoff = call_payoff(
+    sT, higher_strike_price_long_call, premium_higher_strike_long_call
+)
 
 fig, ax = plt.subplots()
-ax.spines['bottom'].set_position('zero')
-ax.plot(sT,higher_strike_long_call_payoff, color='g')
-ax.set_title('LONG 35 Strike Call')
-plt.xlabel('Stock Price (sT)')
-plt.ylabel('Profit & Loss')
+ax.spines["bottom"].set_position("zero")
+ax.plot(sT, higher_strike_long_call_payoff, color="g")
+ax.set_title("LONG 35 Strike Call")
+plt.xlabel("Stock Price (sT)")
+plt.ylabel("Profit & Loss")
 
 plt.show()
 
 # Short Call Payoff
-Short_call_payoff = call_payoff(sT, strike_price_short_call, premium_short_call)*-1.0
+Short_call_payoff = call_payoff(sT, strike_price_short_call, premium_short_call) * -1.0
 
 fig, ax = plt.subplots()
-ax.spines['bottom'].set_position('zero')
-ax.plot(sT, Short_call_payoff, color='r')
-ax.set_title('Short 32.5 Strike Call')
-plt.xlabel('Stock Price')
-plt.ylabel('Profit & Loss')
+ax.spines["bottom"].set_position("zero")
+ax.plot(sT, Short_call_payoff, color="r")
+ax.set_title("Short 32.5 Strike Call")
+plt.xlabel("Stock Price")
+plt.ylabel("Profit & Loss")
 
 plt.show()
 
-Butterfly_spread_payoff = lower_strike_long_call_payoff + higher_strike_long_call_payoff + 2 *Short_call_payoff
+Butterfly_spread_payoff = (
+    lower_strike_long_call_payoff
+    + higher_strike_long_call_payoff
+    + 2 * Short_call_payoff
+)
 
 fig, ax = plt.subplots()
-ax.spines['bottom'].set_position('zero')
-ax.plot(sT,Butterfly_spread_payoff ,color='b', label= 'Butterfly Spread')
-ax.plot(sT, lower_strike_long_call_payoff,'--', color='g',label='Lower Strike Long Call')
-ax.plot(sT, higher_strike_long_call_payoff,'--', color='g', label='Higher Strike Long Call')
-ax.plot(sT, Short_call_payoff, '--', color='r', label='Short call')
+ax.spines["bottom"].set_position("zero")
+ax.plot(sT, Butterfly_spread_payoff, color="b", label="Butterfly Spread")
+ax.plot(
+    sT, lower_strike_long_call_payoff, "--", color="g", label="Lower Strike Long Call"
+)
+ax.plot(
+    sT, higher_strike_long_call_payoff, "--", color="g", label="Higher Strike Long Call"
+)
+ax.plot(sT, Short_call_payoff, "--", color="r", label="Short call")
 plt.legend()
-plt.xlabel('Stock Price')
-plt.ylabel('Profit & Loss')
+plt.xlabel("Stock Price")
+plt.ylabel("Profit & Loss")
 
 plt.show()
 
-Butterfly_spread_payoff = lower_strike_long_call_payoff + higher_strike_long_call_payoff + 2 *Short_call_payoff
+Butterfly_spread_payoff = (
+    lower_strike_long_call_payoff
+    + higher_strike_long_call_payoff
+    + 2 * Short_call_payoff
+)
 
 fig, ax = plt.subplots()
-ax.spines['bottom'].set_position('zero')
-ax.plot(sT,Butterfly_spread_payoff ,color='b', label= 'Butterfly Spread')
+ax.spines["bottom"].set_position("zero")
+ax.plot(sT, Butterfly_spread_payoff, color="b", label="Butterfly Spread")
 plt.legend()
-plt.xlabel('Stock Price')
-plt.ylabel('Profit & Loss')
+plt.xlabel("Stock Price")
+plt.ylabel("Profit & Loss")
 
 plt.show()
 
 profit = max(Butterfly_spread_payoff)
 loss = min(Butterfly_spread_payoff)
-print ("%.2f" %profit)
-print ("%.2f" %loss)
+print("%.2f" % profit)
+print("%.2f" % loss)
