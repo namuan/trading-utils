a1b451fa60b34abf045c6d70b4381c5d78bf29e5
namuan <575441+namuan@users.noreply.github.com>
2024-08-09 12:42:35 +0100

    refactor: More optimisation. Remove unrealised profit/loss

--- options-ironfly-payoff.py
+++ options-ironfly-payoff.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 
 
@@ -14,31 +14,24 @@ class OptionContract:
     def payoff(self, stock_prices):
         if self.contract_type == "call":
             payoff = (
-                    np.where(
-                        stock_prices > self.strike_price,
-                        stock_prices - self.strike_price,
-                        0,
-                        )
-                    - self.premium
+                np.where(
+                    stock_prices > self.strike_price,
+                    stock_prices - self.strike_price,
+                    0,
+                )
+                - self.premium
             )
         else:  # put
             payoff = (
-                    np.where(
-                        stock_prices < self.strike_price,
-                        self.strike_price - stock_prices,
-                        0,
-                        )
-                    - self.premium
+                np.where(
+                    stock_prices < self.strike_price,
+                    self.strike_price - stock_prices,
+                    0,
+                )
+                - self.premium
             )
 
         return payoff * 100 * (1 if self.position == "long" else -1)
-
-    def unrealized_pl(self):
-        return (
-                (self.current_premium - self.premium)
-                * 100
-                * (-1 if self.position == "long" else 1)
-        )
 
 
 class IronButterfly:
@@ -48,9 +41,6 @@ class IronButterfly:
 
     def calculate_payoff(self, strike_range):
         return sum(option.payoff(strike_range) for option in self.options)
-
-    def calculate_unrealized_pl(self):
-        return sum(option.unrealized_pl() for option in self.options)
 
 
 class Trade:
@@ -72,9 +62,6 @@ class Trade:
             ib.calculate_payoff(self.strike_range) for ib in self.iron_butterflies
         )
 
-    def calculate_total_unrealized_pl(self):
-        return sum(ib.calculate_unrealized_pl() for ib in self.iron_butterflies)
-
     def find_total_breakeven_points(self):
         total_payoff = self.calculate_total_payoff()
         return self.strike_range[np.where(np.diff(np.sign(total_payoff)) != 0)[0]]
@@ -83,30 +70,69 @@ class Trade:
         total_payoff = self.calculate_total_payoff()
         max_profit = np.max(total_payoff)
         max_loss = np.min(total_payoff)
-        unrealized_pl = self.calculate_total_unrealized_pl()
         print(f"Trade Statistics:")
         print(f"Max Profit: ${max_profit:.2f}")
         print(f"Max Loss: ${max_loss:.2f}")
-        print(f"Unrealized Profit/Loss: ${unrealized_pl:.2f}")
-        return max_profit, max_loss, unrealized_pl
+        return max_profit, max_loss
 
     def plot_trade(self):
+        plotter = TradePlot(self)
+        plotter.plot()
+
+
+class TradePlot:
+    def __init__(self, trade):
+        self.trade = trade
+        self.fig, self.ax = plt.subplots(figsize=(14, 8))
         sns.set(style="whitegrid")
-        fig, ax = plt.subplots(figsize=(14, 8))
 
-        total_payoff = self.calculate_total_payoff()
+    def plot(self):
+        self._setup_plot()
+        self._plot_payoff()
+        self._plot_strike_lines()
+        self._plot_breakeven_points()
+        self._annotate_max_loss()
+        self._add_legend()
+        plt.tight_layout()
+        plt.show()
 
-        ax.plot(self.strike_range, total_payoff, label="Trade Payoff", linewidth=1)
-        ax.fill_between(
-            self.strike_range,
+    def _setup_plot(self):
+        self.ax.set_xlim(self.trade.strike_range[0], self.trade.strike_range[-1])
+        self.ax.set_ylim(
+            min(self.trade.calculate_total_payoff()) * 1.1,
+            max(self.trade.calculate_total_payoff()) * 1.1,
+        )
+        self.ax.spines["top"].set_visible(False)
+        self.ax.spines["right"].set_visible(False)
+        self.ax.spines["bottom"].set_position("zero")
+        self.ax.set_ylabel("Profit/Loss ($)", fontsize=10)
+        self.ax.set_xticks(
+            np.arange(self.trade.strike_range[0], self.trade.strike_range[-1], 50)
+        )
+        self.ax.set_xticklabels(
+            np.arange(self.trade.strike_range[0], self.trade.strike_range[-1], 50),
+            rotation=45,
+            ha="right",
+            fontsize=8,
+        )
+        self.ax.tick_params(axis="y", labelsize=8)
+        self.ax.grid(True, linestyle=":", alpha=0.7)
+
+    def _plot_payoff(self):
+        total_payoff = self.trade.calculate_total_payoff()
+        self.ax.plot(
+            self.trade.strike_range, total_payoff, label="Trade Payoff", linewidth=1
+        )
+        self.ax.fill_between(
+            self.trade.strike_range,
             total_payoff,
             0,
             where=(total_payoff > 0),
             facecolor="lightgreen",
             alpha=0.5,
         )
-        ax.fill_between(
-            self.strike_range,
+        self.ax.fill_between(
+            self.trade.strike_range,
             total_payoff,
             0,
             where=(total_payoff < 0),
@@ -114,104 +140,122 @@ class Trade:
             alpha=0.5,
         )
 
-        ax.set_xlim(self.strike_range[0], self.strike_range[-1])
-        ax.set_ylim(min(total_payoff) * 1.1, max(total_payoff) * 1.1)
-        ax.spines["top"].set_visible(False)
-        ax.spines["right"].set_visible(False)
-        ax.spines["bottom"].set_position("zero")
-
-        ax.set_ylabel("Profit/Loss ($)", fontsize=10)
-        ax.text(
-            0.95,
-            0.95,
-            "Trade Payoff",
-            transform=ax.transAxes,
-            fontsize=14,
-            fontweight="bold",
-            ha="right",
-            va="top",
-        )
-
-        for ib in self.iron_butterflies:
+    def _plot_strike_lines(self):
+        for ib in self.trade.iron_butterflies:
             for option in ib.options:
-                ax.axvline(x=option.strike_price, color="skyblue", linestyle="--")
-                ax.text(
+                self.ax.axvline(x=option.strike_price, color="skyblue", linestyle="--")
+                self.ax.text(
                     option.strike_price,
-                    ax.get_ylim()[1],
+                    self.ax.get_ylim()[1],
                     f"{option.strike_price}",
                     color="skyblue",
                     ha="center",
                     va="bottom",
                     fontsize=8,
                 )
-
-        ax.axvline(
-            x=self.iron_butterflies[0].spot_price,
+        self.ax.axvline(
+            x=self.trade.iron_butterflies[0].spot_price,
             color="black",
             linestyle=":",
             linewidth=1.5,
         )
 
-        ax.set_xticks(np.arange(self.strike_range[0], self.strike_range[-1], 50))
-        ax.set_xticklabels(
-            np.arange(self.strike_range[0], self.strike_range[-1], 50),
-            rotation=45,
-            ha="right",
-            fontsize=8,
-        )
-        ax.tick_params(axis="y", labelsize=8)
-        ax.grid(True, linestyle=":", alpha=0.7)
+    def _plot_breakeven_points(self):
+        breakeven_points = self.trade.find_total_breakeven_points()
+        center_strike = self.trade.iron_butterflies[0].spot_price
 
-        breakeven_points = self.find_total_breakeven_points()
         for point in breakeven_points:
-            ax.annotate(
-                f"{point:.2f}",
-                xy=(point, 0),
-                xycoords="data",
-                xytext=(0, 30),
-                textcoords="offset points",
-                arrowprops=dict(
-                    arrowstyle="->", connectionstyle="arc3,rad=.2", color="red"
-                ),
-                ha="center",
-                va="bottom",
-                color="red",
-                fontsize=10,
-            )
+            self._annotate_breakeven(point, center_strike)
 
-        # Annotate max loss on each side
-        left_max_loss = np.min(total_payoff[: len(total_payoff) // 2])
-        right_max_loss = np.min(total_payoff[len(total_payoff) // 2 :])
-        left_max_loss_index = np.argmin(total_payoff[: len(total_payoff) // 2])
-        right_max_loss_index = (
-                np.argmin(total_payoff[len(total_payoff) // 2 :]) + len(total_payoff) // 2
+    def _annotate_breakeven(self, point, center_strike):
+        percent_diff = ((point - center_strike) / center_strike) * 100
+
+        # Determine the direction of the line
+        direction = 1 if point > center_strike else -1
+
+        # Calculate the y-coordinate for the line
+        y_coord = self.ax.get_ylim()[1] * 0.2 * direction
+
+        # Draw the horizontal dotted line with arrows
+        self.ax.annotate(
+            "",
+            xy=(center_strike, y_coord),
+            xytext=(point, y_coord),
+            arrowprops=dict(
+                arrowstyle="<->", linestyle=":", color="purple", linewidth=0.5
+            ),
         )
 
-        ax.annotate(
-            f"Max Loss: ${left_max_loss:.2f}",
-            xy=(self.strike_range[left_max_loss_index], left_max_loss),
-            xytext=(-40, -20),
+        # Add percentage text above the line
+        midpoint = (center_strike + point) / 2
+        self.ax.text(
+            midpoint,
+            y_coord + direction * 5,
+            f"{abs(percent_diff):.2f}%",
+            ha="center",
+            va="bottom" if direction > 0 else "top",
+            color="purple",
+            fontsize=10,
+        )
+
+        # Add vertical dotted lines
+        self.ax.vlines(
+            x=center_strike,
+            ymin=0,
+            ymax=y_coord,
+            color="purple",
+            linestyle=":",
+            linewidth=0.5,
+        )
+        self.ax.vlines(
+            x=point, ymin=0, ymax=y_coord, color="purple", linestyle=":", linewidth=0.5
+        )
+
+        # Annotate the breakeven point
+        self.ax.annotate(
+            f"Breakeven: {point:.2f}",
+            xy=(point, 0),
+            xytext=(0, 30 * direction),
             textcoords="offset points",
-            arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2", color="red"),
-            ha="right",
-            va="bottom",
+            ha="center",
+            va="bottom" if direction > 0 else "top",
             color="red",
             fontsize=10,
         )
 
-        ax.annotate(
-            f"Max Loss: ${right_max_loss:.2f}",
-            xy=(self.strike_range[right_max_loss_index], right_max_loss),
-            xytext=(50, -30),
+    def _annotate_max_loss(self):
+        total_payoff = self.trade.calculate_total_payoff()
+        left_max_loss = np.min(total_payoff[: len(total_payoff) // 2])
+        right_max_loss = np.min(total_payoff[len(total_payoff) // 2 :])
+        left_max_loss_index = np.argmin(total_payoff[: len(total_payoff) // 2])
+        right_max_loss_index = (
+            np.argmin(total_payoff[len(total_payoff) // 2 :]) + len(total_payoff) // 2
+        )
+
+        self.ax.annotate(
+            f"Max Loss: ${left_max_loss:.2f}",
+            xy=(self.trade.strike_range[left_max_loss_index], left_max_loss),
+            xytext=(-10, -50),
             textcoords="offset points",
-            arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=-.2", color="red"),
             ha="left",
             va="bottom",
             color="red",
             fontsize=10,
         )
 
-        max_profit, max_loss, unrealized_pl = self.print_trade_statistics()
+        self.ax.annotate(
+            f"Max Loss: ${right_max_loss:.2f}",
+            xy=(self.trade.strike_range[right_max_loss_index], right_max_loss),
+            xytext=(10, -50),
+            textcoords="offset points",
+            ha="right",
+            va="bottom",
+            color="red",
+            fontsize=10,
+        )
+
+    def _add_legend(self):
+        max_profit, max_loss = self.trade.print_trade_statistics()
         legend_elements = [
             plt.Line2D(
                 [0],
@@ -232,27 +276,16 @@ class Trade:
             plt.Line2D(
                 [0],
                 [0],
-                color="w",
-                marker="o",
-                markerfacecolor="black",
-                label=f"Unrealized P/L: ${unrealized_pl:.2f}",
-            ),
-            plt.Line2D(
-                [0],
-                [0],
                 color="black",
                 linestyle=":",
-                label=f"Spot Price: {self.iron_butterflies[0].spot_price}",
+                label=f"Spot Price: {self.trade.iron_butterflies[0].spot_price}",
             ),
         ]
-        ax.legend(handles=legend_elements, fontsize=8, loc="upper left")
-
-        plt.tight_layout()
-        plt.show()
+        self.ax.legend(handles=legend_elements, fontsize=8, loc="upper left")
 
 
 def main():
-    spot_price = 5355
+    spot_price = 5319
     options1 = [
         OptionContract(5420, 15.10, 22.50, "call", "long"),
         OptionContract(5235, 94.30, 124.50, "call", "short"),
@@ -271,7 +304,12 @@ def main():
     iron_butterfly1 = IronButterfly(spot_price, options1)
     iron_butterfly2 = IronButterfly(spot_price, options2)
 
-    trade = Trade([iron_butterfly1, iron_butterfly2])
+    trade = Trade(
+        [
+            iron_butterfly1,
+            iron_butterfly2,
+        ]
+    )
     trade.plot_trade()
 
 
