f3d6b45ca6b174d351b36014bd09b3ff42a4320a
namuan <575441+namuan@users.noreply.github.com>
2024-08-14 09:41:21 +0100

    feat: Ironfly adjustments

--- ib_ironfly_another_fly_adjustment.py
+++ ib_ironfly_another_fly_adjustment.py
@@ -8,12 +8,13 @@ Port 7496 is for connection to TWS using real trading account
 Port 4002 is for connection to IB Gateway using paper trading account
 Port 4001 is for connection to IB Gateway using real trading account
 
-Simulate adjusting an IronFly position by adding an ATM Straddle
+Simulate adjusting an IronFly position by adding another ATM IronFly
 Assumptions:
 ES Options
 Update Expiry date (Line 59) to pick up the positions from the account
 """
 from collections import defaultdict
+from typing import List
 
 from ib_insync import *
 
@@ -54,6 +55,23 @@ def es_premium(n):
     return round(result, 2)
 
 
+def get_mid_price(bid, ask):
+    return bid + ask / 2
+
+
+def calculate_total_premium(options: List[Ticker]) -> float:
+    total_premium = 0
+    for option in options:
+        mid_price = get_mid_price(option.bid, option.ask)
+        total_premium += mid_price
+    return total_premium
+
+
+def calculate_breakeven_on_each_side(premium_received, strike):
+    return ((round(strike - premium_received, -1)),
+            (round(strike + premium_received, -1)))
+
+
 # Print the grouped positions
 open_contracts = []
 expiry_date = "20240816"
@@ -75,29 +93,44 @@ contract = Future("ES", exchange="CME", lastTradeDateOrContractMonth="202409")
 [ticker] = ib.reqTickers(*[contract])
 spot_price = ticker.marketPrice()
 
-# Apply ATM Straddle Adjustments
+# Apply another ATM IronFly Adjustments
 nearest_strike = round(spot_price, -1)
-print(f"{spot_price=} {nearest_strike=}")
-put_contract = FuturesOption(
+short_put_contract = FuturesOption(
     symbol="ES",
     lastTradeDateOrContractMonth=expiry_date,
     strike=nearest_strike,
     right="P",
 )
-call_contract = FuturesOption(
+short_call_contract = FuturesOption(
     symbol="ES",
     lastTradeDateOrContractMonth=expiry_date,
     strike=nearest_strike,
     right="C",
 )
-new_contracts = ib.reqTickers(*(ib.qualifyContracts(*[put_contract, call_contract])))
+new_short_contracts = ib.reqTickers(
+    *(ib.qualifyContracts(*[short_put_contract, short_call_contract]))
+)
+total_premium_received = calculate_total_premium(new_short_contracts)
+breakeven_low, breakeven_high = calculate_breakeven_on_each_side(
+    total_premium_received, nearest_strike
+)
+long_put_contract = FuturesOption(
+    symbol="ES",
+    lastTradeDateOrContractMonth=expiry_date,
+    strike=breakeven_low,
+    right="P",
+)
+long_call_contract = FuturesOption(
+    symbol="ES",
+    lastTradeDateOrContractMonth=expiry_date,
+    strike=breakeven_high,
+    right="C",
+)
+new_long_contracts = ib.reqTickers(
+    *(ib.qualifyContracts(*[long_put_contract, long_call_contract]))
+)
 
-
-def get_mid_price(bid, ask):
-    return bid + ask / 2
-
-
-for con in new_contracts:
+for con in new_short_contracts:
     open_contracts.append(
         OptionContract(
             strike_price=con.contract.strike,
@@ -107,10 +140,20 @@ for con in new_contracts:
         )
     )
 
+for con in new_long_contracts:
+    open_contracts.append(
+        OptionContract(
+            strike_price=con.contract.strike,
+            premium=get_mid_price(con.bid, con.ask),
+            contract_type="call" if con.contract.right == "C" else "put",
+            position="long",
+        )
+    )
+
 for c in open_contracts:
     print(c)
 
 amendment = OptionPlot(open_contracts, spot_price)
-amendment.plot("Current Position with ATM Straddle")
+amendment.plot("Current Position with another ATM IronFly")
 
 ib.disconnect()
