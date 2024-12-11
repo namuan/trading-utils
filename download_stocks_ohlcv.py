ebabfeb7448cff0a2aea381ae8829e5ae90efe9e
namuan <575441+namuan@users.noreply.github.com>
2024-12-11 11:00:55 +0000

    feat: Make it easy to run this script

--- download_stocks_ohlcv.py
+++ download_stocks_ohlcv.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 """
 Download stocks open, close, high, low and volume data for all available stocks.
 Make sure you have downloaded the list of tickers using download_stocklist.py script.
 """
+
 from argparse import ArgumentParser
 from datetime import datetime
 
-from common.market import download_tickers_data
-from common.market import load_all_tickers
+from common.market import download_tickers_data, load_all_tickers
 
 
 def parse_args():
