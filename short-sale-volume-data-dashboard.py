ea84ad5728597df8049e1a589ae47f44d0610dc5
namuan <575441+namuan@users.noreply.github.com>
2025-01-02 22:05:45 +0000

    refactor: Consolidate logging setup

--- short-sale-volume-data-dashboard.py
+++ short-sale-volume-data-dashboard.py
@@ -26,25 +26,8 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+from common.logger import setup_logging
 from common.market_data import download_ticker_data
-
-
-def setup_logging(verbosity):
-    logging_level = logging.WARNING
-    if verbosity == 1:
-        logging_level = logging.INFO
-    elif verbosity >= 2:
-        logging_level = logging.DEBUG
-
-    logging.basicConfig(
-        handlers=[
-            logging.StreamHandler(),
-        ],
-        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging_level,
-    )
-    logging.captureWarnings(capture=True)
 
 
 def parse_args():
--- short-sale-volume-data-importer.py
+++ short-sale-volume-data-importer.py
@@ -23,23 +23,7 @@ from sqlite3 import IntegrityError
 
 import pandas as pd
 
-
-def setup_logging(verbosity):
-    logging_level = logging.WARNING
-    if verbosity == 1:
-        logging_level = logging.INFO
-    elif verbosity >= 2:
-        logging_level = logging.DEBUG
-
-    logging.basicConfig(
-        handlers=[
-            logging.StreamHandler(),
-        ],
-        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging_level,
-    )
-    logging.captureWarnings(capture=True)
+from common.logger import setup_logging
 
 
 def parse_args():
