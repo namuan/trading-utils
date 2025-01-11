a98d52fecb8c979b4fedef9fa6db20aabd1e7436
namuan <575441+namuan@users.noreply.github.com>
2025-01-11 21:31:20 +0000

    chore: Reuse

--- options-price-plot.py
+++ options-price-plot.py
@@ -24,23 +24,7 @@ from pathlib import Path
 import pandas as pd
 import plotly.graph_objects as go
 
-
-def setup_logging(verbosity):
-    logging_level = logging.WARNING
-    if verbosity == 1:
-        logging_level = logging.INFO
-    elif verbosity >= 2:
-        logging_level = logging.DEBUG
-
-    logging.basicConfig(
-        handlers=[
-            logging.StreamHandler(),
-        ],
-        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging_level,
-    )
-    logging.captureWarnings(capture=True)
+from common.logger import setup_logging
 
 
 def parse_args():
